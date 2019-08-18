d35500d003a076e39c735e195f819da20ab8a09c
Adeyemi Salau <adeyemisalau@gmail.com>
2019-08-18 04:30:55 +0100

    added web interface to benchmark

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const fastify = require('fastify')({
   logger: true
 });
 
+const path = require('path')
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/', // optional: default '/'
+})
+
+fastify.get('/benchmarking', function (req, reply) {
+  reply.sendFile('benchmark.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
+
 // Require external modules
 const mongoose = require('mongoose')
 
@@ -16,6 +27,8 @@ const swagger = require('./config/swagger')
 // Register Swagger
 fastify.register(require('fastify-swagger'), swagger.options)
 
+fastify.register(require('fastify-formbody'))
+
 // Connect to DB
 mongoose.connect('mongodb+srv://desire:OI9PR1QIRDYBiYPB@brandsng-k5x55.mongodb.net/cart?retryWrites=true&w=majority')
  .then(() => console.log('MongoDB connected…'))
@@ -23,6 +36,30 @@ mongoose.connect('mongodb+srv://desire:OI9PR1QIRDYBiYPB@brandsng-k5x55.mongodb.n
 
 routes.forEach((route, index) => {
  fastify.route(route)
+})
+
+const autocannon = require('autocannon')
+
+var jQuery = require("jquery");
+
+// async/await
+function benchMark (c, p, d) {
+  const result = autocannon({
+    url: 'https://nodejs-rest-cart-api.herokuapp.com/api/carts',
+    connections: c, //default
+    pipelining: p, // default
+    duration: d // default
+  })
+  return result
+}
+
+// Declare a route
+fastify.post('/benchmark', async (request, reply) => {
+    var c = request.body.d
+    var p = request.body.p
+    var d = request.body.d
+    let res = await benchMark(c,p,d);
+    reply.send(res)
 })
 
 // Initiate Concurrency
